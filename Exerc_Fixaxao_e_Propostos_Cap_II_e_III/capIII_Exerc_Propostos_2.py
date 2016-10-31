4c698b4f89885c05b708b81816d32541fae203c8
motax30 <motax30@gmail.com>
2016-10-31 14:38:41 -0200

    Add files via upload

---
+++ Exerc_Fixaxao_e_Propostos_Cap_II_e_III/capIII_Exerc_Propostos_2.py
@@ -0,0 +1,5 @@
+print("Algoritmo cálculo área de um círculo com um raio fornecido")
+pi = 3.14
+r = int(input("Informe um raio para cálculo da área do círculo:"))
+area = 2*pi*(r**2)
+print("O valor da área do círculo é: %d"%area)
