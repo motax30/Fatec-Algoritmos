286bf9740d7029f89646218be744b999f0c5a0d2
adriano <motax30@gmail.com>
2016-11-10 00:39:27 -0200

    Corrigido a finalização do laço de verificação de peça fora do Tabuleiro

--- Ep2_Batalha_Naval/Jogo_Batalha_Naval.py
+++ Ep2_Batalha_Naval/Jogo_Batalha_Naval.py
@@ -3,10 +3,9 @@ arq1 = open('jogador1.txt','r').readlines()
 arq2 = open('jogador2.txt','r').readlines()
 j1 = []
 j2 = []
-i=0
+
 pecasPosicionadas_j1 = ''
 pecasPosicionadas_j2 = ''
-
 col = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"]
 
 def preencherTabuleiroVazio(posicoes):
@@ -14,23 +13,26 @@ def preencherTabuleiroVazio(posicoes):
         for j in range(1, 16):
             posicoes+= col[l] + str(j) + ",0|"
     return posicoes
+
 tabul_j1 = preencherTabuleiroVazio(pecasPosicionadas_j1)
 tabul_j2 = preencherTabuleiroVazio(pecasPosicionadas_j2)
 
-while i<=3:
-    cod_j1 = (arq1[i].split("; "))[0].split(";")[0]
-    posicao_direcao_j1 = (arq1[i].split("; "))[0].split(";")[1].replace("\n","").split("|")
-    j = [cod_j1]
-    for p in range(len(posicao_direcao_j1)):
-        j.append(posicao_direcao_j1[p])
-    j1.append(j)
-    cod_j2 = (arq2[i].split("; "))[0].split(";")[0]
-    posicao_direcao_j2 = (arq2[i].split("; "))[0].split(";")[1].replace("\n", "").split("|")
-    k = [cod_j2]
-    for p in range(len(posicao_direcao_j2)):
-        k.append(posicao_direcao_j2[p])
-    j2.append(k)
-    i+=1
+def extrairPecasDosArquivos():
+    i = 0
+    while i<=3:
+        cod_j1 = (arq1[i].split("; "))[0].split(";")[0]
+        posicao_direcao_j1 = (arq1[i].split("; "))[0].split(";")[1].replace("\n","").split("|")
+        j = [cod_j1]
+        for p in range(len(posicao_direcao_j1)):
+            j.append(posicao_direcao_j1[p])
+        j1.append(j)
+        cod_j2 = (arq2[i].split("; "))[0].split(";")[0]
+        posicao_direcao_j2 = (arq2[i].split("; "))[0].split(";")[1].replace("\n", "").split("|")
+        k = [cod_j2]
+        for p in range(len(posicao_direcao_j2)):
+            k.append(posicao_direcao_j2[p])
+        j2.append(k)
+        i+=1
 
 def getIndicePecaJogador(jogador,termoPesquisado):
     for j in range(len(jogador)):
@@ -38,7 +40,7 @@ def getIndicePecaJogador(jogador,termoPesquisado):
                 if termoPesquisado == jogador[j][i]:
                     return j,i
 
-def montarPecas(codPeca, posPeca, horientacao):
+def getElementosDaPeca(codPeca, posPeca, horientacao):
     qtd_Encouracados,qtd_portaAvioes,qtd_Submarinos,qtd_Cruzadores = 2,2,5,4
     elem = ""
     elementos = ""
@@ -52,43 +54,43 @@ def montarPecas(codPeca, posPeca, horientacao):
         linhaPeca = posPeca[0]
         colPeca = int(posPeca[1]+posPeca[2])
     if horientacao=="H":
-        if codPeca == qtd_Encouracados:
-            while i<4:
+        if codPeca == 1:
+            while i<qtd_Encouracados:
                 elem+=(linhaPeca+str(colPeca)+",1|")
                 i+=1
                 colPeca+=1
             elementos = elem
-        elif codPeca == qtd_portaAvioes:
-            while i<5:
+        elif codPeca == 2:
+            while i<qtd_portaAvioes:
                 elem+=(linhaPeca+str(colPeca)+",1|")
                 i+=1
                 colPeca+=1
             elementos = elem
-        elif codPeca == qtd_Submarinos:
+        elif codPeca == 3:
             linhaPeca = posPeca[0]
             colPeca= int(posPeca[1])
             elementos = (linhaPeca+str(colPeca)+",1|")
-        elif codPeca == qtd_Cruzadores:
-            while i<4:
+        elif codPeca == 4:
+            while i<qtd_Cruzadores:
                 elem+=(linhaPeca+str(colPeca)+",1|")
                 i+=1
                 colPeca+=1
             elementos = elem
     elif horientacao=="V":
         colLetra=col.index(linhaPeca)
-        if codPeca == qtd_Encouracados:
-            while i<2:
+        if codPeca == 1:
+            while i<qtd_Encouracados:
                 elem+=(col[colLetra]+str(colPeca)+",1|")
                 i+=1
                 colLetra+=1
             elementos = elem
-        elif codPeca == qtd_portaAvioes:
-            while i<5:
+        elif codPeca == 2:
+            while i< qtd_portaAvioes:
                 elem += (col[colLetra] + str(colPeca) + ",1|")
                 i += 1
                 colLetra += 1
             elementos = elem
-        elif codPeca == qtd_Submarinos:
+        elif codPeca == 3:
             elementos = (col[colLetra] + str(colPeca) + ",1|")
         elif codPeca == qtd_Cruzadores:
             while i<4:
@@ -101,9 +103,8 @@ def montarPecas(codPeca, posPeca, horientacao):
         elementos = (col[colLetra] + str(colPeca) + ",1|")
     return elementos
 
-def posicionarPecasJogador(jogador,tabuleiro):      #Passar como parâmetro a lista das posições do Jogador: J1 ou J2
-    tabuleiro
-    for c in range(len(jogador)):
+def posicionarPecasJogador(jogador,tabuleiroVazio):      #Passar como parâmetro a lista das posições do Jogador: J1 ou J2
+    for c in range(0,len(jogador)):
         cod = getIndicePecaJogador(jogador, jogador[c][0])
         for d in range(0,len(jogador[c])-1):
             peca = getIndicePecaJogador(jogador,jogador[c][d+1])
@@ -112,27 +113,40 @@ def posicionarPecasJogador(jogador,tabuleiro):      #Passar como parâmetro a li
                 pos = pos[len(pos)-1:]
             else:
                 pos = 'N/A'
-            peca= montarPecas(int(jogador[cod[0]][cod[1]]), jogador[peca[0]][peca[1]], pos).split('|')
-            f=0
-            while f<len(peca)-1:
-                global tabul_j1
-                if len(peca[f])<5:
-                    velho  = peca[f][:len(peca[f])-2]+",0"
-                    novo = peca[f]
-                else:
-                    velho = peca[f][:len(peca[f]) - 2] + ",0"
-                    novo = peca[f]
-                if velho in tabul_j1:
-                    tabul_j1 = tabul_j1.replace(velho, novo)
-                else:
-                    print("Posição %s está fora dos limites do tabuleiro.Esta posição pertence à peça %s"
-                          ""%(velho[:-2],peca[0][:-2]+pos))
-                    f=len(peca)-1
-                    d=len(jogador[c])-1
-                    c=len(jogador[c])
-                f+=1
-    return tabul_j1
-tabuleiro_j1 = posicionarPecasJogador(j1,pecasPosicionadas_j1)
-tabuleiro_j2 = posicionarPecasJogador(j2,pecasPosicionadas_j2)
-print(tabuleiro_j1)
-print(tabuleiro_j2)
+            peca= getElementosDaPeca(int(jogador[cod[0]][cod[1]]), jogador[peca[0]][peca[1]], pos).split('|')
+            adicionarElementosPecaNoTabuleiro(peca,tabuleiroVazio,pos)
+
+def adicionarElementosPecaNoTabuleiro(peca,tabuleiroVazio,posicao):
+    f = 0
+    while f < len(peca) - 1:
+        global temp
+        if len(peca[f]) < 5:
+            velho = peca[f][:len(peca[f]) - 2] + ",0"
+            novo = peca[f]
+        else:
+            velho = peca[f][:len(peca[f]) - 2] + ",0"
+            novo = peca[f]
+        if tabuleiroVazio == tabul_j1:
+            if velho in tabul_j1:
+                k = temp.replace(velho, novo)
+                temp = k
+        elif tabuleiroVazio == tabul_j2:
+            if velho in tabul_j2:
+                k = temp.replace(velho, novo)
+                temp = k
+        else:
+            print("Posição %s está fora dos limites do tabuleiro.Esta posição pertence à peça %s"
+                  "" % (velho[:-2], peca[0][:-2] + posicao))
+            return False
+            break
+        f+=1
+
+temp = tabul_j1
+extrairPecasDosArquivos()
+posicionarPecasJogador(j1,tabul_j1)
+pecasPosicionadas_j1= temp
+temp = tabul_j1
+posicionarPecasJogador(j2,tabul_j2)
+pecasPosicionadas_j2= temp
+print(pecasPosicionadas_j1)
+print(pecasPosicionadas_j2)
